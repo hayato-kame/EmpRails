9df96fe8400bc5849f85b8ecc5da6d6e0bcf0aa5
hayato-kao <rsz42916@nifty.com>
2021-05-16 16:11:43 +0900

    login

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,4 +1,14 @@
 class ToppagesController < ApplicationController
+  layout 'application'
+  before_action :authenticate_account!, only: :login_check
+
   def index
+
+  end
+
+  def login_check
+    @account = current_account
+    pp @account
+    binding.pry
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   devise_for :accounts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'toppages#index'
+  get 'toppages/index'
+  get 'toppages', to: 'toppages#index'
+  get 'toppages/login_check', to: 'toppages#login_check'
 
   get 'departments/index'
   get 'departments',  to: 'departments#index'
