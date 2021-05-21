aab3422b0c78bda527f5f047b8550a1fa22dc053
hayato-kao <rsz42916@nifty.com>
2021-05-21 12:05:38 +0900

    employee seed OK

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,28 +1,42 @@
 class Employee < ApplicationRecord
-  belongs_toは、従データ側に書く
+  # belongs_toは、従データ側に書く
   belongs_to :department
   belongs_to :photo
 
 
-  validates :name, :age, :gender, :zipz_number, :pref, :address1, :address2, :address3, :department_id, :hire_date, {presence: {message: 'は、必須項目です。'}}
+  validates :name, :age, :gender, :zip_number, :pref, :address1, :address2, :address3, :department_id, :hire_date, {presence: {message: 'は、必須項目です。'}}
   validates :name, {length: {maximum: 255, message: 'は、255文字以内で入力してください' }}
-  validates :age, {only_integer: {message: 'は、整数値を入れてください。'}, greater_than_or_equal_to: {0, message: 'は、０以上です。'}, less_than: {200, message: 'は、200未満です。'} }
-  # 日付のバリデーションをカスタマイズする
-  validate :pretend_ago
+  # validates :age, {only_integer: {message: 'は、整数値を入れてください。'}, greater_than_or_equal_to: {0, message: 'は、０以上です。'}, less_than: {200, message: 'は、200未満です。'} }
+  # validates :age, numericality: {only_integer: {message: 'は、整数値を入れてください。'}, greater_than_or_equal_to: 0, less_than_or_equal_to: 200}
+
+  validates :age, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 200}
+  # 日付のバリデーションをカスタマイズする 入社日は、必須項目であればいい、上でつけてる 退社日は、独自のバリデーション
   validate :retire
 
-  def pretend_ago #メソッド名は任意
-    # errors.add(:date型のカラム名, '表示させたいエラーメッセージ') if date型のカラム名.nil? || date型のカラム名 < Date.today
-    errors.add(:hire_date, 'は、今日以前の日付を入力してください') if :hire_date.nil? || :hire_date <= Date.today
-  end
-
-  def retire #メソッド名は任意  退社日は、 nullも許可されてるので && なおかつ   を使う
-    # errors.add(:date型のカラム名, '表示させたいエラーメッセージ') if date型のカラム名.nil? && date型のカラム名 < Date.today
+  def retire #メソッド名は任意  退社日は、 nullも許可されてるので && なおかつ   を使う 未入力だったらスルー
     # もし、退職日が入力してあって、退職日が、入社日よりも前だったら、エラーメッセージ 退職日は、null許容だから
     if :retire_date && :retire_date < :hire_date
     errors.add(:retire_date, 'は、入社日以降の日付を入力してください')
     end
   end
+
+# シードファイル employee_seed.rbファイル で使う  クラスメソッド 静的メソッド インスタンス化しなくても呼び出せるメソッド
+# self.メソッド名で定義する
+def self.get_random(n)
+    hiragana = ["ぁ","あ","ぃ","い","ぅ","う","ぇ","え","ぉ","お",
+      "か","が","き","ぎ","く","ぐ","け","げ","こ","ご",
+      "さ","ざ","し","じ","す","ず","せ","ぜ","そ","ぞ",
+      "た","だ","ち","ぢ","っ","つ","づ","て","で","と","ど",
+      "な","に","ぬ","ね","の","は","ば","ぱ",
+      "ひ","び","ぴ","ふ","ぶ","ぷ","へ","べ","ぺ","ほ","ぼ","ぽ",
+      "ま","み","む","め","も","ゃ","や","ゅ","ゆ","ょ","よ",
+      "ら","り","る","れ","ろ","ゎ","わ","ゐ","ゑ","を","ん"]
+    str = hiragana.sample(n)
+    # binding.pry
+    return str
+  end
+
+
 
 
 end
--- db/seeds/employee_seed.rb
+++ db/seeds/employee_seed.rb
@@ -1,57 +1,51 @@
 # 子テーブル側なので、テテなし子にならないように、親テーブルphotosに、同じ数だけデータが必要です
 # 12データ作成します
 
-Employee.create(
+# まず、３つ作った。createメソッドの引数には、配列を用意して、１データをハッシュにする 3つのハッシュで3データ作成
+Employee.create([
   {employee_id: 'EMP0001', name: '山田 太郎', age: 35, gender: '男', photo_id: 1, zip_number: '100-0001', pref: '東京都',
   address1: '千代田区', address2: '千代田', address3: 'ちよだ', department_id: 'D01', hire_date: '2000-11-11', retire_date: nil},
-  {employee_id: 'EMP0002', name: '日本 花子', age: 24, gender: '女', photo_id: ２, zip_number: '330-0841', pref: '埼玉県',
+  {employee_id: 'EMP0002', name: '日本 花子', age: 24, gender: '女', photo_id: 2, zip_number: '330-0841', pref: '埼玉県',
     address1: 'さいたま市', address2: '大宮区', address3: '東町', department_id: 'D03', hire_date: '2010-12-12', retire_date: '2017-09-07'},
   {employee_id: 'EMP0003', name: '東京 次郎', age: 45, gender: '男', photo_id: 3, zip_number: '251-0013', pref: '神奈川県',
     address1: '川崎市', address2: '麻生区', address3: '王禅寺', department_id: 'D03', hire_date: '1999-05-21', retire_date: nil}
-)
+])
 
+
+# 後の９つのデータをループで作成
 fnames = ["伊藤", "山本", "中村", "小林"];
 gnames = ["三郎", "四郎", "友子"];
-pref_aray = ["東京都", "千葉県", "茨城県", "神奈川県"];
+pref_array = ["東京都", "千葉県", "茨城県", "神奈川県"];
 dep_id_array = ["D01", "D02", "D03"];
 
-
-
-(1..9).times do |n|
-
-  str_emp_id = sprintf("EMP%04d", (n + 3) )  #これで  EMP0004 から  EMP0012  まで作れる
-  e_name = "${fnames[n % 4]}" + " " + "${gnames[n % 3]"
+9.times do |n| # ブロック引数のnは、0から8まで渡される
+  str_emp_id = sprintf("EMP%04d", (n + 4) )  #これで  EMP0004 から  EMP0012  まで作れる
+  e_name = "#{fnames[(n+1) % 4]} #{gnames[(n+1) % 3]}"
   e_gender = ""
-  if name.include?("友子")　then
+  if e_name.include?("友子")
     e_gender = "女"
   else
     e_gender = "男"
   end
-  str_zip_number = sprintf("%03d", n*3) + sprintf("%04d", n*4)
-  random_address = self.get_ramdom(5)
-  h_date = Time.new.strftime("%Y-%m-%d")
-  r_date = (h_date + n.days).strftime("%Y-%m-%d")
-  # date = date.strftime("%Y-%m-%d")
-
-
+  str_zip_number = sprintf("%03d", (n+1)*3) + '-' + sprintf("%04d", (n+1)*4)
+  random_address_array = Employee.get_random(5)
+  # 配列の要素を連結して文字列化する
+  e_address = random_address_array.join
+  h_date = (Time.local(2017,7,5)+ 24*60*60*n).strftime("%Y-%m-%d")
+  r_date = (Time.now - 24*60*60*n).strftime("%Y-%m-%d")
   Employee.create(
-    {employee_id: :str_emp_id, name: :e_name, age: rand(18..80), gender: :e_gender, photo_id: n + 3, zip_number: :str_zip_number,
-    pref: pref_array[n % 3], address1: :random_address, address2: :random_address, address3: :random_address, department_id: dep_id_array[n % 3],
-    hire_date: :h_date, retire_date: :r_date}
+    { employee_id: str_emp_id,
+      name: e_name,
+      age: rand(18..80),
+      gender: e_gender,
+      photo_id: n + 4,
+      zip_number: str_zip_number,
+      pref: pref_array[(n+1) % 3],
+      address1: e_address,
+      address2: e_address,
+      address3: e_address,
+      department_id: dep_id_array[(n+1) % 3],
+      hire_date: h_date,
+      retire_date: r_date }
   )
-
-
-end
-
-private
-def get_ramdom(n)
-  hiragana = ["ぁ","あ","ぃ","い","ぅ","う","ぇ","え","ぉ","お",
-    "か","が","き","ぎ","く","ぐ","け","げ","こ","ご",
-    "さ","ざ","し","じ","す","ず","せ","ぜ","そ","ぞ",
-    "た","だ","ち","ぢ","っ","つ","づ","て","で","と","ど",
-    "な","に","ぬ","ね","の","は","ば","ぱ",
-    "ひ","び","ぴ","ふ","ぶ","ぷ","へ","べ","ぺ","ほ","ぼ","ぽ",
-    "ま","み","む","め","も","ゃ","や","ゅ","ゆ","ょ","よ",
-    "ら","り","る","れ","ろ","ゎ","わ","ゐ","ゑ","を","ん"]
-  str = hiragana.sample(n)
 end
